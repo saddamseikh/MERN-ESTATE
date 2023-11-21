34118b8bc2f3f2a14229d7f05a0a2809ff17c503
saddamseikh <saddamseikh2229@gmail.com>
2023-11-21 19:19:07 +0530

    Create Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
