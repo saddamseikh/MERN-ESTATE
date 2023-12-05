86b7a3e14f426e91e84c91fd8a3e313c37efcb5e
saddam seikh <saddamseikh2229@gmail.com>
2023-12-05 21:14:46 +0530

    Create signIn page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [fromData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...fromData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(fromData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data);
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  return (
+    <div className=" p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold p-3"> Loging Page </h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="border p-3 rounded-lg text-center"
+          placeholder="Enter Your email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter Your password"
+          className="border p-3 rounded-lg text-center"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading...." : "Sign In"}
+        </button>
+      </form>
+
+      <div className=" flex gap-2 ">
+        <p>You have not account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">SignUp</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
