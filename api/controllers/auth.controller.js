7de66ab152bc3da072386252041f42fb41d8ea6b
saddam seikh <saddamseikh2229@gmail.com>
2023-12-05 21:10:02 +0530

    Signin api error handle

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
@@ -19,18 +19,18 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-
     // User validation check using email
-    if (!validUser) return;
-    next(errorHandler(404, "User not found"));
+    if (!validUser) {
+      return next(errorHandler(404, "Invalid Users"));
+    }
 
     // compare database hashSync password to text password
     const validPassword = bcryptjs.compareSync(password, validUser.password);
 
     // Password validation check
-    if (!validPassword) return;
-    next(errorHandler(401, "Invalid Password"));
-
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Password"));
+    }
     // password sundefined so it does not get sent in the response
     validUser.password = undefined;
 
@@ -38,11 +38,13 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
     // token save inside the cookie withname access_token
-    res
-      .cookie("access_token", token, { httpOnly: true })
-      .status(200)
-      .json(validUser);
-  } catch (e) {
-    next(error);
+    res.cookie("access_token", token, { httpOnly: true });
+    res.status(200).json({
+      success: true,
+      message: "User login successfully",
+      validUser,
+    });
+  } catch (error) {
+    return next(errorHandler(500, e.message));
   }
 };
